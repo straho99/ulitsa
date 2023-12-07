b6f05f58a3ada3efd08c7090ccbd67e5d3e11da2
StrahilRuychev <strahil.ruychev@gmail.com>
2023-12-07 11:47:28 +0200

    Solution

--- ulitsa/main.cpp
+++ ulitsa/main.cpp
@@ -6,9 +6,36 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    int countNotCrossed, countNotCrossedEven = 0, countNotCrossedOdd = 0, currentNumber;
+    int maxEven = 0, maxOdd = 0;
+    
+    cin >> countNotCrossed;
+    
+    for(int i = 1; i <= countNotCrossed; i++) {
+        cin >> currentNumber;
+        
+        if (currentNumber % 2 == 0) {
+            countNotCrossedEven++;
+            
+            if (currentNumber > maxEven) {
+                maxEven = currentNumber;
+            }
+        } else {
+            countNotCrossedOdd++;
+            
+            if (currentNumber>maxOdd) {
+                maxOdd = currentNumber;
+            }
+        }
+    }
+    
+    int totalEven = maxEven / 2;
+    int totalOdd = maxOdd / 2 + 1;
+    
+    cout << (totalOdd - countNotCrossedOdd) << " " << (totalEven - countNotCrossedEven);
+    
     return 0;
 }
