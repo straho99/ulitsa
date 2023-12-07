d37a202350367b56811e8941db519a88588e7e80
StrahilRuychev <strahil.ruychev@gmail.com>
2023-12-07 11:32:04 +0200

    Initial Commit

---
+++ ulitsa/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ulitsa
+//
+//  Created by Strahil Ruychev on 7.12.23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
